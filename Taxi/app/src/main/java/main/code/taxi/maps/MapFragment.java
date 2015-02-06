adcc908a3585a0feba732cf93ee736aaffc89663
steffanboodhoo <steffan_boodhoo@hotmail.com>
2015-02-05 23:03:31 -0400

    seperated into fragment

--- Taxi/app/src/main/java/main/code/taxi/maps/MapFragment.java
+++ Taxi/app/src/main/java/main/code/taxi/maps/MapFragment.java
@@ -1,18 +1,34 @@
 package main.code.taxi.maps;
 
 import android.app.Fragment;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 import main.code.taxi.taxi.R;
 
 /**
  * Created by Steffan on 05/02/2015.
  */
-public class MapFragment extends Fragment{
+public class MapFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     View view;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private String mLastUpdateTime;
+    private Location mCurrentLocation;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -20,6 +36,56 @@ public class MapFragment extends Fragment{
         MapFragment mapFragment = (MapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
 //        mapFragment.getMapAsync(this);
+        buildGoogleApiClient();
+        mGoogleApiClient.connect();
         return view;
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(getActivity(), "connected", Toast.LENGTH_SHORT).show();
+        createLocationRequest();
+        //if (mRequestingLocationUpdates) {
+        startLocationUpdates();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(500);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        updateUI();
+    }
+    private void updateUI() {
+        Toast.makeText(getActivity(),""+String.valueOf(mCurrentLocation.getLatitude()),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
--- Taxi/app/src/main/java/main/code/taxi/taxi/Main.java
+++ Taxi/app/src/main/java/main/code/taxi/taxi/Main.java
@@ -14,20 +14,13 @@ import android.widget.Toast;
 
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.Socket;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-
-import java.text.DateFormat;
-import java.util.Date;
 
 import main.code.taxi.maps.MapFragment;
 
 public class Main extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks{
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
@@ -78,17 +71,11 @@ public class Main extends ActionBarActivity
 
             Log.d("main","Data emitted");
         }catch (Exception e){e.printStackTrace();};*/
-        buildGoogleApiClient();
-        mGoogleApiClient.connect();
+
+
     }
 
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
+
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
@@ -148,42 +135,4 @@ public class Main extends ActionBarActivity
         }
     };
 
-    @Override
-    public void onConnected(Bundle bundle) {
-        Toast.makeText(this,"connected",Toast.LENGTH_SHORT).show();
-        createLocationRequest();
-        //if (mRequestingLocationUpdates) {
-            startLocationUpdates();
-        //}
-    }
-    protected void startLocationUpdates() {
-        LocationServices.FusedLocationApi.requestLocationUpdates(
-                mGoogleApiClient, mLocationRequest, this);
-    }
-    protected void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(1000);
-        mLocationRequest.setFastestInterval(500);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
-    @Override
-    public void onLocationChanged(Location location) {
-        mCurrentLocation = location;
-        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
-        updateUI();
-    }
-
-    private void updateUI() {
-        Toast.makeText(this,""+String.valueOf(mCurrentLocation.getLatitude()),Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Toast.makeText(this,"connection failed",Toast.LENGTH_SHORT).show();
-    }
 }
